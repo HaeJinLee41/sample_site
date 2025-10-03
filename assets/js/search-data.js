d01f6cdabf9d08b9e6dc11bca3e15d8a5375d2c5
HaeJinLee41 <32312632+HaeJinLee41@users.noreply.github.com>
2025-10-03 21:16:14 +0000

    Deploying to gh-pages from @ HaeJinLee41/sample_site@c9e8463ecd80aa7217b7378750bfc1e354b57231 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/sample_site/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/sample_site/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-journal-paper-calibration-discrepancy-predicts-students-subsequent-metacognitive-strategy-use-in-computer-based-learning-has-been-accepted-to-the-international-journal-of-artificial-intelligence-in-education",
+          title: 'My journal paper “Calibration discrepancy predicts students’ subsequent metacognitive strategy use in computer-based...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
