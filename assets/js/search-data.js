5ecddeba2fcbe6f55c6de648779b95afa538de82
HaeJinLee41 <32312632+HaeJinLee41@users.noreply.github.com>
2025-10-09 17:29:50 +0000

    Deploying to gh-pages from @ HaeJinLee41/sample_site@f07965291dd8ef21188434de478144624e79c5ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,7 +399,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-my-journal-paper-calibration-discrepancy-predicts-students-subsequent-metacognitive-strategy-use-in-computer-based-learning-has-been-accepted-to-the-international-journal-of-artificial-intelligence-in-education",
+          section: "News",},{id: "news-my-journal-paper-calibration-discrepancy-predicts-students-subsequent-metacognitive-strategy-use-in-computer-based-learning-has-been-accepted-to-the-international-journal-of-artificial-intelligence-in-education-international-journal-of-artificial-intelligence-in-education-hj",
           title: 'My journal paper, “Calibration discrepancy predicts students’ subsequent metacognitive strategy use in computer-based...',
           description: "",
           section: "News",},{id: "projects-project-1",
