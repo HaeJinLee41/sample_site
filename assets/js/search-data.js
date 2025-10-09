77587df90d879a43e31d620ad8d8924fc668fb5b
HaeJinLee41 <32312632+HaeJinLee41@users.noreply.github.com>
2025-10-09 21:29:38 +0000

    Deploying to gh-pages from @ HaeJinLee41/sample_site@d10c6a19783987161030e711b2b6ed7e7a043137 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,14 +452,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%61%65%6A%69%6E%32@%69%6C%6C%69%6E%6F%69%73.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/haejin-lee-331226285", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=eJsun0AAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
