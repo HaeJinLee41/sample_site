c6a56145176329915d5fa2fdbfa377fc870f0633
HaeJinLee41 <32312632+HaeJinLee41@users.noreply.github.com>
2025-10-15 05:38:57 +0000

    Deploying to gh-pages from @ HaeJinLee41/site@02ae502e6f6ce0ed6b568bfc8a322705df48f829 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HaeJinLee41/sample_site",
+    "data-repo": "HaeJinLee41/site",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
